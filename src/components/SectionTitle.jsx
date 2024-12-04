fe94d985efa355cea34e2730391ae143833c118e
SuraCdac <sura.cdac@gmail.com>
2024-12-04 07:01:08 +0530

    updated sectionTitle.jsx

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SectionTitle(props) {
   return (
     <div className="text-center m-5 p-4">
-      <div className="text-7xl opacity-15 leading-3 pt-6 mt-3 font-sectiontitle font-bold">{props.title}</div>
+      <div className="text-7xl opacity-30 leading-3 pt-6 mt-3 font-sectiontitle font-bold">{props.title}</div>
       <div className="text-5xl font-sectiontitle font-bold">{props.title}</div>
       <div className="m-3 p-3 text-xl font-slabo">{props.desc}</div>
     </div>
