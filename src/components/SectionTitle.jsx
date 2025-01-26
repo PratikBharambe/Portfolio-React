a1b95baafe8ff617e054bece49697a72bbecc530
SuraCdac <sura.cdac@gmail.com>
2025-01-26 10:19:52 +0530

    remove connected

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,18 +1,3 @@
-// import React from "react";
-
-// function SectionTitle(props) {
-//   return (
-//     <div className="text-center m-5 p-4">
-//       <div className="text-7xl opacity-30 leading-3 pt-6 mt-3 font-charm font-bold tracking-wider">{props.title}</div>
-//       <div className="text-5xl font-charm font-bold tracking-wide">{props.title}</div>
-//       <div className="m-3 p-3 text-xl font-slabo">{props.desc}</div>
-//     </div>
-//   );
-// }
-
-// export default SectionTitle;
-
-
 import React from "react";
 
 function SectionTitle(props) {
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import bg from "./assets/images/bg.jpg";
-// import Navbar from "./components/Navbar";
-// import Intro from "./components/Intro";
-
-// function Home() {
-//   return (
-//     <>
-//       <div className="w-full h-screen" style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover", backgroundPosition: "center"}}>
-//         <div className="w-full h-screen" style={{ background: "rgba(0, 0, 0, 0.6)"}}>
-//             <Navbar/>
-//             <div>
-//                 <Intro />
-//             </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Home;
-
 import React from "react";
 import bg from "./assets/images/bg.jpg";
 import Navbar from "./components/Navbar";
